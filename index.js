15a138cb0195560df2ff88d96e175b70ff1e006a
AnderGI <agibarguen@gmail.com>
2022-12-10 16:52:13 +0100

    Recursive Fibonacci function refactored

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
  function fibsRec(num, prev = 0, current = 1){
-    if(num == 0){ 
-        //the first two values of the fibonacci
-        //secuence are prev and current
-        //that's why it does it six times
+    /**
+     *if(num == 2){ 
+        the first two values of the fibonacci
+        secuence are prev and current
+        that's why it does it six times
         return current;
     }else{
         return fibsRec(num - 1, current, current + prev)
     }
+     */
+    num === 2 ? current : fibsRec(num - 1, current, current + prev);
  }
 
  console.log(fibsRec(8))
