edb1357439812635b148d0cce64a12d7423a5c5a
AnderGI <agibarguen@gmail.com>
2022-12-10 17:18:05 +0100

    Recursive Fibonacci function with array

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
- function fibsRec(num, prev = 0, current = 1){
-    /**
-     *if(num == 2){ 
-        the first two values of the fibonacci
-        secuence are prev and current
-        that's why it does it six times
-        return current;
-    }else{
-        return fibsRec(num - 1, current, current + prev)
-    }
-     */
-    num === 2 ? current : fibsRec(num - 1, current, current + prev);
- }
+function fibsRec(num, prev = 0, current = 1, array = Array.from([0])) {
+  /**
+   * Array.from() to pass the value of [] which would be an objet into an array
+   */
+  return num == 1 ? array : fibsRec(num - 1, current, current + prev, array.concat(current));
+}
 
- console.log(fibsRec(8))
+console.log(fibsRec(8));
